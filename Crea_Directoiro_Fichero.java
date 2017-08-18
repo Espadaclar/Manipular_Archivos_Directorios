c49128be74552e41f22bdf67537825fc7988a6ad
Espadaclar <eneroborde8002@gmail.com>
2017-08-18 19:33:50 +0200

    Crea un archivo dentro de un directorio. video 160.

--- Crea_Directoiro_Fichero.java
+++ Crea_Directoiro_Fichero.java
@@ -11,9 +11,16 @@ public class Crea_Directoiro_Fichero
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // RUTA DONDE SE CREA EL DIRECTORIO.
-        File ruta = new File("C:" +File.separator+ "Users" +File.separator+ "Usuario" +File.separator+ "Desktop" +File.separator+ "Nueva_Car2");
-        ruta.mkdir();
+        // RUTA DONDE SE CREA EL ARCHIVO.
+        File ruta = new File("C:" +File.separator+ "Users" +File.separator+ "Usuario" +File.separator+
+                                    "Desktop" +File.separator+ "Nueva_Car2" +File.separator+ "relojes2.txt");
+        try {
+            ruta.createNewFile();
+        } catch (IOException ex) {
+            System.out.println("herror.");
+        }
+        //ruta.mkdir();
+        
     }
     
 }
